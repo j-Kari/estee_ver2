b7ad853686f082d9beb0737432f9025fc471bda8
Kiran K <rogrdat@outlook.com>
2023-07-09 16:41:14 -0400

    Initial Commit

---
+++ estee_ver2/estee_ver2App.swift
@@ -0,0 +1,17 @@
+//
+//  estee_ver2App.swift
+//  estee_ver2
+//
+//  Created by Kiran Kari on 7/9/23.
+//
+
+import SwiftUI
+
+@main
+struct estee_ver2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
